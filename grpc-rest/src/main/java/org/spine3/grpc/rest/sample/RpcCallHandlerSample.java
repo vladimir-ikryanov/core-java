482fc24f381f0d21ae0ac4bf482f538dc6cef121
Mikhail Mikhaylov <mikhail.mikhaylov@teamdev.com>
2015-11-13 14:40:00 +0200

    Implement base classes for generated code. Write sample implementations for these base classes.

---
+++ grpc-rest/src/main/java/org/spine3/grpc/rest/sample/RpcCallHandlerSample.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2015, TeamDev Ltd. All rights reserved.
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.spine3.grpc.rest.sample;
+
+//<generated imports>
+
+import com.google.protobuf.Message;
+import org.spine3.grpc.rest.RpcCallHandler;
+
+/**
+ * Sample class which shows the desired result of a code generation tool.
+ */
+//<generated name>
+public class RpcCallHandlerSample implements RpcCallHandler<Message, Message> {
+    @Override
+    public Message handle(Message requestMessage) {
+        throw new IllegalStateException("Please implement this handler first.");
+    }
+}
